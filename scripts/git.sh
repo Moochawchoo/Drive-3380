c5b596529b75de0357fcff5f51ae6e48636b2b96
Unknown <rzhong9963@gmail.com>
2019-02-10 03:09:06 -0600

    Update script

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,8 +3,8 @@
 echo "\n"
 git clone https://github.com/brahndun/drive_3380.git /home/rz/drive_3380 && echo "CLONED\n"
 
-mv -u /home/rz/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
+rsync -r /home/rz/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
 
 rm -rf /home/rz/drive_3380 && echo "GIT REPO DELETED\n"
 
-rm -rf README.md .git && echo "EXCESS FILES DELETED"
+rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
