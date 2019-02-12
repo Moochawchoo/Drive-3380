fdd7a8d48e1e4779fd23b48cbbd6228cb778c445
Unknown <rzhong9963@gmail.com>
2019-02-12 17:17:09 -0600

    Once again. Update script

--- scripts/git.sh
+++ scripts/git.sh
@@ -7,14 +7,10 @@ grep -Eo "1[0-9]{9}" /home/git/drive_3380/.git/logs/refs/heads/master > /home/gi
 
 chmod 777 /home/git/timestamps/gitcommit
 
-remote=$(cat /home/git/timestamps/gitcommit)
-slocal=$(cat /home/git/timestamps/lastcommit)
-
-if [ $remote -gt $slocal ]; then
+if [ $(cat /home/git/timestamps/lastcommit) -gt $(cat /home/git/timestamps/gitcommit) ]; then
     rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
     rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED\n"
     rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
-    rm -rf /home/git/timestamps/lastcommit
     date +%s > /home/git/timestamps/lastcommit
     chmod 777 /home/git/timestamps/lastcommit
 else
