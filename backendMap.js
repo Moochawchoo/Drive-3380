bf384995521e1d51951a07498ab38180a80ec178
Moochawchoo <45804533+Moochawchoo@users.noreply.github.com>
2019-02-11 20:28:43 -0600

    Update backendMap.js
    created div header for map

--- backendMap.js
+++ backendMap.js
@@ -1,7 +1,9 @@
 var map;
 
+<div id="map"></div>
+
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById("map"), {
           center: {lat: 30.4133, lng: 91.1800},
           zoom: 8
         });
