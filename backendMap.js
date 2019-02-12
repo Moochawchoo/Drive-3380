67ac9f715b9187a1c1721d36c1805aadb4c1f2c6
Brandon Norman <b.norman12397@gmail.com>
2019-02-11 20:17:28 -0600

    Update backendMap.js

--- backendMap.js
+++ backendMap.js
@@ -1,8 +1,8 @@
+var map;
 
-  var uluru = {lat: -25.344, lng: 131.036};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
