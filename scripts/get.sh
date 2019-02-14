bf3d01e9c3d99c9ef8a7ce3a70aa5fb3aba49ac0
Rob <rzhong9963@gmail.com>
2019-02-14 17:18:20 -0600

    Update script to work with bot

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/brahndun/drive_3380.git /home/git/drive_3380 && echo "CLONED\n"
+git clone https://github.com/brahndun/drive_3380.git /home/git/drive_3380 && echo "CLONED"
 
 grep -Eo "1[0-9]{9}" /home/git/drive_3380/.git/logs/refs/heads/master > /home/git/timestamps/gitcommit
 
 chmod 777 /home/git/timestamps/gitcommit
 
 if [ $(cat /home/git/timestamps/lastcommit) -gt $(cat /home/git/timestamps/gitcommit) ]; then
-    rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
-    rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED\n"
+    rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED"
+    rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED"
     rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
     date +%s > /home/git/timestamps/lastcommit
     chmod 777 /home/git/timestamps/lastcommit
 else
-    rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
-    rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED\n"
+    rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED"
+    rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED"
     rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
 fi
