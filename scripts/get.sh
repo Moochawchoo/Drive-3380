ffe2afdecfe00c718b5edf324fcab4867ae3e3c6
Rob <rzhong9963@gmail.com>
2019-02-14 01:22:33 -0600

    Update script

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "\n"
 git clone https://github.com/brahndun/drive_3380.git /home/git/drive_3380 && echo "CLONED\n"
 
 grep -Eo "1[0-9]{9}" /home/git/drive_3380/.git/logs/refs/heads/master > /home/git/timestamps/gitcommit
@@ -14,16 +13,7 @@ if [ $(cat /home/git/timestamps/lastcommit) -gt $(cat /home/git/timestamps/gitco
     date +%s > /home/git/timestamps/lastcommit
     chmod 777 /home/git/timestamps/lastcommit
 else
-    read -p "Overwrite files: " option
-    if [ $option=="y" ]; then
-        rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
-        rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED\n"
-        rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
-        date +%s > /home/git/timestamps/lastcommit
-        chmod 777 /home/git/timestamps/lastcommit
-    else
-        echo "Nothing to be overwritten. Deleting repo."
-        rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED\n"
-        rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
-    fi
+    rsync -r /home/git/drive_3380/* /var/www/html/ && echo "FILES MOVED\n"
+    rm -rf /home/git/drive_3380 && echo "GIT REPO DELETED\n"
+    rm -rf /var/www/html/README.md /var/www/html/.git && echo "EXCESS FILES DELETED"
 fi
