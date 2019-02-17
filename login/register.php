68cdf52b949ab3223730c73431ec189708ef1268
Unknown <rzhong9963@gmail.com>
2019-02-16 22:54:42 -0600

    Update register

--- login/register.php
+++ login/register.php
@@ -29,7 +29,7 @@ else { // Email doesn't already exist in a database, proceed...
 
     // active is 0 by DEFAULT (no need to include it here)
     $sql = "INSERT INTO users (first_name, last_name, email, password, hash) " 
-            . "VALUES ('$first_name','$last_name','$email','$password', '$hash', '1')";
+            . "VALUES ('$first_name','$last_name','$email','$password', '$hash', '1');";
 
     // Add user to the database
     if ( $mysqli->query($sql) ){
