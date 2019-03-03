7d51847027dd2e5dcade05a0486a6271656ba5d9
Rob <rzhong9963@gmail.com>
2019-03-03 02:46:32 -0600

    Revert "Reorganize Structure"

--- login/index.php
+++ login/index.php
@@ -1,43 +1,36 @@
 <?php
-$title = 'Home';
-
-include "login/misc/pagehead.php";
+$userrole = 'loginpage';
+$title = 'Login';
+include 'misc/pagehead.php';
 ?>
 </head>
 <body>
-
-<?php require 'login/misc/pullnav.php'; ?>
-
-<div class="container">
-
-<?php
-
-if ($auth->isLoggedIn()) {
-    echo '<div class="jumbotron text-center"><h1>Hi, '.$_SESSION['username'].'!</h1>
-    <p>Click on your username in the top right corner to expose menu options</p></div>
-    <div class="col-lg-2"></div><div class="col-lg-8">
-    <h2>Menu Items:</h2>
-
-    <p><b><em>Edit Profile</em></b> - Edit your own user profile information including your name, contact info, avatar, etc</p>
-    <p><b><em>Account Settings</em></b> - Change your email address and/or password</p>';
-
-    if ($auth->isAdmin()) {
-        echo '<p><b><em>Manage Active Users</em></b> - Admin manage active users and/or ban trolls</p>';
-        echo '<p><b><em>Verify/Delete Users</em></b> - Admin mass verify or delete new user requests</p>';
-        echo '<p><b><em>Mail Log</em></b> - Admin mail status logging</p>';
-    }
-
-    if ($auth->isSuperAdmin()) {
-        echo '<p><b><em>Edit Site Config</em></b> - Superadmin edit site configuration in one page</p>';
-    }
-} else {
-    echo '<div class="jumbotron text-center"><h1 class="display-1">Homepage</h1>
-    <small>This is your homepage. You are currently signed out.</small><br><br>
-    <p>You can sign in or create a new account by clicking "Sign In" in the top right corner!</p>';
-}
-
-?>
-        </div><div class="col-lg-2"></div>
+  <?php require 'misc/pullnav.php'; ?>
+    <div class="container logindiv">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4">
+            <form class="text-center" name="loginform" method="post" action="ajax/checklogin.php">
+                <h3 class="form-signin-heading"><?php echo $title;?></h3>
+                <br>
+                <div class="form-group">
+                    <input name="myusername" id="myusername" type="text" class="form-control input-lg" placeholder="Username" autofocus>
+                    <input name="mypassword" id="mypassword" type="password" class="form-control input-lg" placeholder="Password"> </div>
+                <div class="form-group">
+                    <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
+                </div>
+                <div class="form-group">
+                    <div class="checkbox">
+                        <input id="remember"  type="checkbox"> Remember Me</input>
+                    </div>
+                </div>
+            </form>
+            <div id="message"></div>
+            <p class="text-center"><a href="forgotpassword.php">Forgot Password?</a></p>
+            <p class="text-center">or <a href="signup.php">Create an Account</a></p>
+        </div>
+        <div class="col-sm-4"></div>
     </div>
+    <!-- The AJAX login script -->
+    <script src="js/login.js"></script>
 </body>
 </html>
