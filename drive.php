0589a1acc94f17cd8528add5ffecaa85c6d664f0
Robert Zhong <rzhong9963@gmail.com>
2019-03-03 21:07:29 -0600

    Update drive.php
    Redact google API key

--- drive.php
+++ drive.php
@@ -38,7 +38,7 @@ include "login/misc/pagehead.php";
 </head>
 <body>
     <script src="js/backendMap.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBozvdVUpK_GgbLa6t5pGa6MEyWDJtsPm0&callback=initMap" async defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=KEY_REDACTED&callback=initMap" async defer></script>
     <div class="container">
 	<div id="wrapper">
 	    <div id="content">
