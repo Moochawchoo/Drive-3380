0a3a2e8fc3111e2a11e5be44708befd732567c3d
Rob <rzhong9963@gmail.com>
2019-03-11 19:55:02 -0500

    Update

--- login/unused/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'drivedb';
-$pass = 'whatpassword';
-$db = 'accountsTest';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-?>
