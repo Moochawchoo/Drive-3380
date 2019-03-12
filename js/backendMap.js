b7a8c4d14eb0fafdef7e1eed94834d53f3b72efa
Moochawchoo <45804533+Moochawchoo@users.noreply.github.com>
2019-03-11 19:42:41 -0500

    taking out the markers

--- js/backendMap.js
+++ js/backendMap.js
@@ -1,17 +1,9 @@
 var map;
-var rider1 = {lat: 30.41, lng: -91.18};
-var rider2 = {lat: 30.403441, lng: -91.183599};
-var rider3 = {lat: 30.368702, lng: -91.171043};
-var rider4 = {lat: 30.475827, lng: -91.125020 };
 function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
           center: {lat: 30.4133, lng: -91.1800},
           zoom: 14
         });
-		addMarker(rider1, map, 'John');
-		addMarker(rider2, map, 'Jim');
-		addMarker(rider3, map, 'Justin');
-		addMarker(rider4, map, 'Zach');
       }
 
 function addMarker(location, map, name) {
