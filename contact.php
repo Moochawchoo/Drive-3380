02a2c5dfb0548b90a884d5c6148e82f67a3b59c9
Spirituu <46801408+Spirituu@users.noreply.github.com>
2019-03-13 15:06:48 -0500

    Consolidated css; added extra line

--- contact.php
+++ contact.php
@@ -4,6 +4,10 @@ include "login/misc/pagehead.php";
 ?>
 <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 <style>
+	#pstyle{
+		color:#E1E1E1;
+		text-align: center;
+	}
     #footer{
 	    bottom: 0;
 	    right: 0;
@@ -21,9 +25,10 @@ include "login/misc/pagehead.php";
 </head>
 <body>
     <h1>
-        <p style="color:#E1E1E1; text-align: center; ;">Contact Us</p>
-        <p style="color:#E1E1E1; text-align: center; ;">Email:drive@1337ersprime.com</p>
-        <p style="color:#E1E1E1; text-align: center; ;">Phone:(605) 475-6968</p>
+        <p id="pstyle">Contact Us</p>
+        <p id="pstyle">Email: drive@1337ersprime.com</p>
+        <p id="pstyle">Phone: (605) 475-6968</p>
+        <p id="pstyle">PO Box: 192 Columbia St. Boca Raton, FL 33428</p>
     </h1>
     <div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
 </body>
