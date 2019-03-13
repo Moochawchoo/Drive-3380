de61fc6ed34c9ffa09370dea3e2e68a47187f480
Moochawchoo <45804533+Moochawchoo@users.noreply.github.com>
2019-03-13 15:55:51 -0500

    Added map and marker
    This should add a map and marker with location of driver

--- driver/pickup.php
+++ driver/pickup.php
@@ -5,15 +5,24 @@ include "../login/misc/pagehead.php";
 ?>
 <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 <style>
-    #content{
-        position: relative;
-        text-align: center;
-        float: left;
-        font-size: 18; 
+    #map{
+     	top:0%;
+	height:100%;
     }
-    #contentwrap{
-        position: relative;
-        float: center;
+	#wrapper{
+		left:0;
+		right:0;
+		top: 52px;
+		bottom; 15px;
+		position: absolute;
+	}
+    #content{
+            top: 0;
+	    bottom: 0;
+	    left: 0;
+	    right: 0;
+	    overflow: auto;
+	    position: absolute;
     }
     #footer{
 	    bottom: 0;
@@ -31,9 +40,49 @@ include "../login/misc/pagehead.php";
     <?php require '../login/misc/pullnav.php'; ?>
 </head>
 <body>
-    <h1>
-        <p style="color:#E1E1E1; text-align: center; ;">Pickup A Rider</p>
-    </h1>
+    <script src="../js/backendMap.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBozvdVUpK_GgbLa6t5pGa6MEyWDJtsPm0&callback=initMap" async defer></script>
+    <div class="container">
+    <div id="wrapper">
+        <div id="content">
+	<div id="map"></div>
+      <script>
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 30.4133, lng: -91.1800},
+          zoom: 14
+        });
+        infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+	    infoWindo.setPosition(pos);
+            addMarker(pos,map, 'You are here driver');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+    </script>
+    
+        </div>
+    </div>
     <div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
 </body>
 </html>
