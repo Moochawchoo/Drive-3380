506b6ecc6acdc7f0a9ac010cc8ae0ffde090ffd1
Rob <rzhong9963@gmail.com>
2019-03-31 18:08:20 -0500

    Remove options

--- index.php
+++ index.php
@@ -77,20 +77,7 @@ if ($auth->isLoggedIn()) {
     echo '<div class="jumbotron text-center"><h1>Hi, '.$_SESSION['username'].'!</h1>
     <p>Click on your username in the top right corner to expose menu options</p></div>
     <div class="col-lg-2"></div><div class="col-lg-8">
-    <h2>Menu Items:</h2>
-
-    <p><b><em>Edit Profile</em></b> - Edit your own user profile information including your name, contact info, avatar, etc</p>
-    <p><b><em>Account Settings</em></b> - Change your email address and/or password</p>';
-
-    if ($auth->isAdmin()) {
-        echo '<p><b><em>Manage Active Users</em></b> - Admin manage active users and/or ban trolls</p>';
-        echo '<p><b><em>Verify/Delete Users</em></b> - Admin mass verify or delete new user requests</p>';
-        echo '<p><b><em>Mail Log</em></b> - Admin mail status logging</p>';
-    }
-
-    if ($auth->isSuperAdmin()) {
-        echo '<p><b><em>Edit Site Config</em></b> - Superadmin edit site configuration in one page</p>';
-    }
+    
 } else {
     echo '<div class="jumbotron text-center"><h1 class="display-1">Welcome to Drive</h1>
     <small>You are currently signed out.</small><br><br>
