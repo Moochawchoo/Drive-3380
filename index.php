f70f6c9bce3abc35ba7e4ca0d2db79db3c620a85
leslyannew <leslyannew@users.noreply.github.com>
2019-03-31 20:13:32 -0500

    Edit text on home

--- index.php
+++ index.php
@@ -87,7 +87,12 @@ if ($auth->isLoggedIn()) {
 	<div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
     </div>
 <div class="column">
-	<p style="color:#E1E1E1; margin-left: 25px"Drive is the ultimate resource for getting around efficiently. Use our price estimator to see which rideshare service is best for your ride. Don't want to use either? Use our own driver-friendly service instead. We allow our drivers to be in control and pick what rides they want to take. If you're someone who commutes using a rideshare service frequently, you can also schedule rides that repeat at whatever day or time you choose. No matter what you need, we'll make sure you enjoy the ride.</p>
+	<p style="color:#E1E1E1; margin-left: 25px">Drive is the ultimate resource for getting around efficiently.
+    Use our price estimator to see which rideshare service is best for your ride. Don't want to use either?
+    Use our own driver-friendly service instead. We allow our drivers to be in control and pick what rides
+    they want to take. If you're someone who commutes using a rideshare service frequently, you can also
+    schedule rides that repeat at whatever day or time you choose. No matter what you need, we'll make sure
+    you enjoy the ride.</p>
 </div>
 <div class="column">
 	<div id="mapper">
