f45f24ab8ed17240a06444300b85aff961063aee
Moochawchoo <45804533+Moochawchoo@users.noreply.github.com>
2019-03-31 20:34:51 -0500

    fixed missing }

--- js/backendMap.js
+++ js/backendMap.js
@@ -1,41 +1,56 @@
 var mysql = require('mysql');
-var con = mysql.createConnection({
+var con = mysql.createConnection(
+{
 	host:
 	user:
 	password:
 	database:
 });
 var map, infoWindow;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 30.4133, lng: -91.1800},
-        zoom: 14
-    });
-    infoWindow = new google.maps.InfoWindow;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            con.connect(function(err){
-	    if(err) throw err;
-	    var sql = "INSERT INTO member_loc (userid, lat, lng) VALUES ( ,position.coords.latitude, position.coords.longitude)";\
-	    con.query(sql, function(err,result){
-		    if(err) throw err;
-	    });
-con.query("SELECT lat, lng FROM member_loc", function (err, results, feilds){
-	if(err) throw err; 
-	for(var i = 0, i < results.length, i++){
-		var pos{ 
-			lat: results[i].lat,
-			lng: results[i].lng;
+function initMap() 
+{
+	map = new google.maps.Map(document.getElementById('map'), 
+				  {
+		center: {lat: 30.4133, lng: -91.1800},
+		zoom: 14
+	});
+	infoWindow = new google.maps.InfoWindow;
+	if (navigator.geolocation) 
+	{
+	navigator.geolocation.getCurrentPosition(function(position) 
+			{
+			con.connect(function(err)
+				{
+				if(err) throw err;
+				var sql = "INSERT INTO member_loc (userid, lat, lng) VALUES ( ,position.coords.latitude, position.coords.longitude)";
+				con.query(sql, function(err,result)
+					{
+					if(err) throw err;
+				});
+				con.query("SELECT lat, lng FROM member_loc", function (err, results, feilds)
+					{
+					if(err) throw err; 
+					for(var i = 0, i < results.length, i++)
+					{
+						var pos
+						{ 
+							lat: results[i].lat,
+							lng: results[i].lng;
+						}
+					}
+					addMarker(pos,map);
+				});
+			}, 
+				    function() {
+				handleLocationError(true, infoWindow, map.getCenter());
+			}
+				    });
+		} 
+		else
+		{
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
 		}
-			addMarker(pos,map);
-	      });
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
 }
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
