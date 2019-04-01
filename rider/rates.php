a1955568fbade8eb230d123f6c2cdfb86de75f68
Spirituu <ChrisFrom3rdHour@gmail.com>
2019-03-31 19:06:50 -0500

    Fixed formating in rates.php

--- rider/rates.php
+++ rider/rates.php
@@ -9,10 +9,18 @@ include "../login/misc/pagehead.php";
 	    bottom: 0;
 	    right: 0;
 	    left: 0;
-	    position: absolute;
-	    width: 100%;
+	    position: relative;
 	    text-align: center;
 	    font-size: 11px;
+    }
+    #content{
+        bottom: 0;
+        right: 0;
+        left: 0;
+        position: absolute;
+        width: 100%;
+        text-align: center;
+        font-size: 11px;
     }
     html, body{
         font-family: 'Roboto';
@@ -24,30 +32,40 @@ include "../login/misc/pagehead.php";
     <h1>
         <p style="color:#E1E1E1; text-align: center; ;">Our Rates</p>
     </h1>
-
-<p>1. <b>DriveScout</b> (a low-cost service which seats up to two passengers)<br />
-2. <b>DriveSquad</b> (a low-cost service which seats up to four passengers)<br />
-3. <b>DriveParty</b> (a mid-cost service which seats up to eight passengers)</p>
-<h3>Drive Rates: our current rates for these services:</h3>
-<p><strong>DriveScout</strong><br />
-Base Fare: $1.25<br />
-Per Minute: $0.15<br />
-Per Mile: $0.90<br />
-Cancellation Fee: $5<br />
-Service Fee: $1.75</p>
-<p><strong>DriveSquad</strong><br />
-Base Fare: $2<br />
-Per Minute: $0.25<br />
-Per Mile: $1.35<br />
-Cancellation Fee: $5<br />
-Service Fee: $1.75</p>
-<p><strong>DriveParty</strong><br />
-Base Fare: $2.50<br />
-Per Min: $0.30<br />
-Per Mile: $1.80<br />
-Cancellation Fee: $10<br />
-Service Fee: $1.75</p>
-   
+    <div id="content">
+        <p>
+            1. <b>DriveScout</b> (a low-cost service which seats up to two passengers)<br />
+            2. <b>DriveSquad</b> (a low-cost service which seats up to four passengers)<br />
+            3. <b>DriveParty</b> (a mid-cost service which seats up to eight passengers)
+        </p>
+        <h3>
+            Drive Rates: our current rates for these services:
+        </h3>
+        <p>
+            <strong>DriveScout</strong><br />
+            Base Fare: $1.25<br />
+            Per Minute: $0.15<br />
+            Per Mile: $0.90<br />
+            Cancellation Fee: $5<br />
+            Service Fee: $1.75
+        </p>
+        <p>
+            <strong>DriveSquad</strong><br />
+            Base Fare: $2<br />
+            Per Minute: $0.25<br />
+            Per Mile: $1.35<br />
+            Cancellation Fee: $5<br />
+            Service Fee: $1.75
+        </p>
+        <p>
+            <strong>DriveParty</strong><br />
+            Base Fare: $2.50<br />
+            Per Min: $0.30<br />
+            Per Mile: $1.80<br />
+            Cancellation Fee: $10<br />
+            Service Fee: $1.75
+        </p>
+    </div>
     <div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
 </body>
 </html>
