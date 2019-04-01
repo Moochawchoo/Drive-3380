494397537528ac3a77abc7dc92e7cef47efa8441
Brandon Norman <b.norman12397@gmail.com>
2019-03-31 19:26:06 -0500

    formats information on rates page

--- rider/rates.php
+++ rider/rates.php
@@ -38,37 +38,43 @@ include "../login/misc/pagehead.php";
             3. <b>DriveParty</b> (seats up to eight)
         </p>
         <h3>
-            our current rates for our services:
+            The current rates for our services:
         </h3>
         <p>
-        <font size = "6">
+        <font size = "5.5">
             <strong>DriveScout</strong><br />
         </font>
+        <font size = "3.5">
             Base Fare: $1.25<br />
             Per Minute: $0.15<br />
             Per Mile: $0.90<br />
             Cancellation Fee: $5<br />
             Service Fee: $1.75
+        </font>
         </p>
         <p>
-        <font size = "6">
+        <font size = "5.5">
             <strong>DriveSquad</strong><br />
         </font>
+        <font size = "3.5">
             Base Fare: $2<br />
             Per Minute: $0.25<br />
             Per Mile: $1.35<br />
             Cancellation Fee: $5<br />
             Service Fee: $1.75
+        </font>
         </p>
         <p>
-        <font size = "6">
+        <font size = "5.5">
             <strong>DriveParty</strong><br />
         </font>
+        <font size = "3.5">
             Base Fare: $2.50<br />
             Per Min: $0.30<br />
             Per Mile: $1.80<br />
             Cancellation Fee: $10<br />
             Service Fee: $1.75
+        </font>
         </p>
     </div>
     <div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
