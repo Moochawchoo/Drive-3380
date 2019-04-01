3b2a3ba50f43e216bd4d78428c74c2e0c86d22d0
Spirituu <ChrisFrom3rdHour@gmail.com>
2019-03-31 19:00:53 -0500

    Update about.php

--- about.php
+++ about.php
@@ -21,8 +21,100 @@ include "login/misc/pagehead.php";
 </head>
 <body>
     <h1>
-        <p style="color:#E1E1E1; text-align: center; ;">About Drive</p>
+        <p style="color:#E1E1E1; text-align: center; ;">About Drive</p>       
     </h1>
+    <div class="row">
+    <div class="column">
+        <div class="card">
+        <img src="" alt="" style="width:100%">
+         <div class="container">
+            <h2>Hunter Adams</h2>
+            <p class="title">Front-end</p>
+            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p>example@example.com</p>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="" alt="" style="width:100%">
+      <div class="container">
+        <h2>Chris Calvo</h2>
+        <p class="title">Front-end</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="" alt="" style="width:100%">
+      <div class="container">
+        <h2>Leslyanne Warrington</h2>
+        <p class="title">Front-end</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+    </div>
+    <div class="row">
+    <div class="column">
+        <div class="card">
+        <img src="" alt="" style="width:100%">
+         <div class="container">
+            <h2>Brandon Norman</h2>
+            <p class="title">Back-end</p>
+            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p>example@example.com</p>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="" alt="" style="width:100%">
+      <div class="container">
+        <h2>Philip Kerr</h2>
+        <p class="title">Back-end</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="" alt="" style="width:100%">
+      <div class="container">
+        <h2>Sean Wilkinson</h2>
+        <p class="title">Back-end</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+    </div>
+    <div class="row">
+        <div class="column">
+        <div class="card">
+        <img src="" alt="" style="width:100%">
+      <div class="container">
+        <h2>Robert Zhong</h2>
+        <p class="title">Almighty Server-guy</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
     <div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
 </body>
 </html>
