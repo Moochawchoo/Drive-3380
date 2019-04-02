25e5f5b9155fda7f320b18be6f097ff5b424b486
Hunter <huadams.2021@gmail.com>
2019-04-02 15:29:57 -0500

    Revert "Merge branch 'master' of https://github.com/brahndun/drive_3380"
    This reverts commit f740a814716338579802b265502c6fd8e3171a2a, reversing
    changes made to b15b54ae3e3f5ff3d1e166256e5e078c4b4e7cc4.

--- drive.php
+++ drive.php
@@ -38,13 +38,73 @@ include "login/misc/pagehead.php";
 </head>
 <body>
     <script src="js/bundle.js"></script>
-    <script src ="js/backendMap.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBozvdVUpK_GgbLa6t5pGa6MEyWDJtsPm0&callback=initMap" async defer></script>
     <div class="container">
 	<div id="wrapper">
 	    <div id="content">
 		<div id="map">
 		    <script>
+                    var mysql = require('mysql');
+var con = mysql.createConnection(
+{
+	host:
+	user:
+	password:
+	database:
+});
+var map, infoWindow;
+function initMap() 
+{
+	map = new google.maps.Map(document.getElementById('map'), 
+				  {
+		center: {lat: 30.4133, lng: -91.1800},
+		zoom: 14
+	});
+	infoWindow = new google.maps.InfoWindow;
+	if (navigator.geolocation) 
+	{
+	navigator.geolocation.getCurrentPosition(function(position) 
+			{
+			con.connect(function(err)
+				{
+				if(err) throw err;
+				var sql = "INSERT INTO member_loc (userid, lat, lng) VALUES ( ,position.coords.latitude, position.coords.longitude)";
+				con.query(sql, function(err,result)
+					{
+					if(err) throw err;
+				});
+				con.query("SELECT lat, lng FROM member_loc", function (err, results, feilds)
+					{
+					if(err) throw err; 
+					for(var i = 0; i < results.length; i++)
+					{
+						var pos =
+						{ 
+							lat: results[i].lat,
+							lng: results[i].lng
+						};
+					}
+					addMarker(pos,map);
+				});
+			}), 
+				    function() {
+				handleLocationError(true, infoWindow, map.getCenter());
+			}
+				    });
+		} 
+		else
+		{
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+		}
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
 		    </script>
 			</div>
 	    </div>
