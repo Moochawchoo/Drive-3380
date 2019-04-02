6594cdeaa92bf2cef018870c591341268e9ca518
leslyannew <leslyannew@users.noreply.github.com>
2019-04-02 17:20:46 -0500

    Update test.php

--- test.php
+++ test.php
@@ -4,48 +4,97 @@ $userrole="DriverRider";
 include "login/misc/pagehead.php";
 ?>
 <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
     #map{
-	top: 0%;
-	height: 100%;
+    	top: 0%;
+    	height: 100%;
     }
     #wrapper{
- 	left:0;
-	right: 0;
-	top: 52px;
-	bottom: 15px;
-	position: absolute;
+     	left:0;
+    	right: 0;
+    	top: 52px;
+    	bottom: 15px;
+    	position: absolute;
     }
     #content{
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	overflow: auto;
-	position: absolute;
+    	top: 0;
+    	bottom: 0;
+    	left: 0;
+    	right: 0;
+    	overflow: auto;
+    	position: absolute;
     }
+
     #footer{
-	bottom: 0;
-	right: 0;
-	left: 0;
-	position: absolute;
-	width: 100%;
-	text-align: center;
-	font-size: 11px;
+    	bottom: 0;
+    	right: 0;
+    	left: 0;
+    	position: absolute;
+    	width: 100%;
+    	text-align: center;
+    	font-size: 11px;
+    }
+
+    body, html {
+      height: 100%;
+      margin: 0;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .hero-image {
+      background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("assets/cars overhead.jpg");
+      height: 50%;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      position: relative;
+    }
+
+    .hero-text {
+      text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: white;
+    }
+
+    .hero-text button {
+      border: none;
+      outline: 0;
+      display: inline-block;
+      padding: 10px 25px;
+      color: black;
+      background-color: #ddd;
+      text-align: center;
+      cursor: pointer;
+    }
+
+    .hero-text button:hover {
+      background-color: #555;
+      color: white;
     }
 </style>
     <?php require "login/misc/pullnav.php"; ?>
 </head>
-<body style="background-image:url('assets/cars overhead.jpg');">
+<body style>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBozvdVUpK_GgbLa6t5pGa6MEyWDJtsPm0&callback=initMap" async defer></script>
+    <div class="hero-image">
+      <div class="hero-text">
+        <h1>I am John Doe</h1>
+        <p>And I'm a Photographer</p>
+        <button>Hire me</button>
+      </div>
+    </div>
     <div class="container">
 	<div id="wrapper">
 	    <div id="content">
-		<div id="map"></div>
+		<!-- <div id="map"></div> -->
 	    </div>
 	</div>
   <!-- <img src="man driving with gps.jpg" style="width:100%"> -->
-    <div id="footer"><header style="color:#b3b3b3">Copyright &#169 2019 Drive</header></div>
+    <div id="footer"><header style="color:#b3b3b3">Copyright © 2019 Drive</header></div>
     </div>
 </body>
 </html>
