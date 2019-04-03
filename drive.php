475a8c72d1c544818837c57d045e74d4b0cbd1c5
Moochawchoo <45804533+Moochawchoo@users.noreply.github.com>
2019-04-02 19:20:02 -0500

    added $conn->close();

--- drive.php
+++ drive.php
@@ -24,7 +24,7 @@ $sql = "SELECT lat FROM $tbl_member_loc";
 $resultLat = $conn->query($sql);
 $sql = "SELECT lng FROM $tbl_member_loc";
 $resultLng = $conn->query($sql);
-
+$conn->close();
 ?>
 <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 <html>
